79fd26983b9206c710f6b5dad15db5ccc88db30c
MBM000 <bekzodmakhamadjanov@gmail.com>
2022-11-13 19:21:26 +0900

    commit

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Text(
+      questionText,
+      style: TextStyle(fontSize: 28),
+      textAlign: TextAlign.center,
+    );
   }
 }
